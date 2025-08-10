c420f8a9187143e562ac9a274b324f36b6e6e6ec
TheAndi <33332336+TheAndi@users.noreply.github.com>
2025-08-10 23:46:48 +0200

    Update __init__.py

--- components/esphome_i2c_sniffer/__init__.py
+++ components/esphome_i2c_sniffer/__init__.py
@@ -27,11 +27,11 @@ async def to_code(config):
     cg.add(var.set_scl_pin(config[CONF_SCL_PIN]))
     await cg.register_component(var, config)
     if CONF_MSG_SENSOR in config:
-        sens = await text_sensor.new_text_sensor(config[CONF_MSG_SENSOR])
+        sens = await text_sensor.new_text_sensor(config[CONF_MSG_SENSOR], parent=var)
         cg.add(var.register_msg_sensor(sens))
     if CONF_LAST_ADDRESS_SENSOR in config:
-        addr = await sensor.new_sensor(config[CONF_LAST_ADDRESS_SENSOR])
+        addr = await sensor.new_sensor(config[CONF_LAST_ADDRESS_SENSOR], parent=var)
         cg.add(var.register_addr_sensor(addr))
     if CONF_LAST_DATA_SENSOR in config:
-        data = await sensor.new_sensor(config[CONF_LAST_DATA_SENSOR])
+        data = await sensor.new_sensor(config[CONF_LAST_DATA_SENSOR], parent=var)
         cg.add(var.register_data_sensor(data))
