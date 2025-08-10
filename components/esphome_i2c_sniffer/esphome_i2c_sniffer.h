9082169015b7a9cc8acd6f697363062bb1d73016
TheAndi <33332336+TheAndi@users.noreply.github.com>
2025-08-10 21:15:02 +0200

    Update esphome_i2c_sniffer.h

--- components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
+++ components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
@@ -1,22 +1,30 @@
 #pragma once
-#include "esphome.h"
+
+#include "esphome/core/component.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/sensor/sensor.h"
 
 #define MAX_BLOCKS 4
 
-class EsphomeI2cSniffer : public Component, public TextSensor {
+namespace esphome {
+namespace esphome_i2c_sniffer {
+
+class EsphomeI2cSniffer : public Component {
  public:
-  EsphomeI2cSniffer(uint8_t pin_sda, uint8_t pin_scl);
+  void set_sda_pin(uint8_t pin) { sda_pin_ = pin; }
+  void set_scl_pin(uint8_t pin) { scl_pin_ = pin; }
+  void register_msg_sensor(text_sensor::TextSensor *sensor) { msg_sensor_ = sensor; }
+  void register_addr_sensor(sensor::Sensor *sensor) { last_addr_sensor_ = sensor; }
+  void register_data_sensor(sensor::Sensor *sensor) { last_data_sensor_ = sensor; }
+  void set_on_address_callback(std::function<void(uint8_t, const std::vector<uint8_t>&)> cb) { cb_ = cb; }
 
   void setup() override;
   void loop() override;
 
-  void set_last_address_sensor(Sensor *sensor);
-  void set_last_data_sensor(Sensor *sensor);
-  void set_on_address_callback(std::function<void(uint8_t address, const std::vector<uint8_t> &data)> cb);
-
  protected:
   static void IRAM_ATTR on_scl();
   static void IRAM_ATTR on_sda();
+  void publish_blocks_();
 
   struct Block {
     uint8_t addr;
@@ -27,19 +35,21 @@ class EsphomeI2cSniffer : public Component, public TextSensor {
     uint8_t data_len;
   };
 
-  static volatile Block blocks[MAX_BLOCKS];
-  static volatile uint8_t block_count;
-  static volatile bool tr_ready;
-  static volatile uint8_t bit_count;
-  static volatile uint8_t byte_count;
-  static volatile uint8_t data_byte;
-  static volatile bool in_transfer;
-  uint8_t pin_sda_;
-  uint8_t pin_scl_;
+  static volatile Block blocks_[MAX_BLOCKS];
+  static volatile uint8_t block_count_;
+  static volatile bool tr_ready_;
+  static volatile uint8_t bit_count_;
+  static volatile uint8_t byte_count_;
+  static volatile uint8_t data_byte_;
+  static volatile bool in_transfer_;
+  uint8_t sda_pin_;
+  uint8_t scl_pin_;
 
-  Sensor *last_address_sensor_;
-  Sensor *last_data_sensor_;
-  std::function<void(uint8_t address, const std::vector<uint8_t> &data)> cb_;
-
-  void print_blocks();
+  text_sensor::TextSensor *msg_sensor_{nullptr};
+  sensor::Sensor *last_addr_sensor_{nullptr};
+  sensor::Sensor *last_data_sensor_{nullptr};
+  std::function<void(uint8_t, const std::vector<uint8_t>&)> cb_;
 };
+
+}  // namespace esphome_i2c_sniffer
+}  // namespace esphome
