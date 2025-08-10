9aa480d22ec54f337aa2622dc3884fb2dbc0ceea
TheAndi <33332336+TheAndi@users.noreply.github.com>
2025-08-10 23:41:30 +0200

    Update esphome_i2c_sniffer.h

--- components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
+++ components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/text_sensor/text_sensor.h"
-#include "esphome/components/sensor/sensor.h"
+#include "esphome/text_sensor/text_sensor.h"
+#include "esphome/sensor/sensor.h"
 
 #define MAX_BLOCKS 4
 
