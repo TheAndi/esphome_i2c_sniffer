280d37fbdec2b7002672053fb63ad7ea83f66590
TheAndi <33332336+TheAndi@users.noreply.github.com>
2025-08-14 00:05:23 +0200

    Update esphome_i2c_sniffer.h

--- components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
+++ components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
@@ -5,8 +5,8 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/log.h"
-#include "esphome/text_sensor/text_sensor.h"
-#include "esphome/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace esphome_i2c_sniffer {
@@ -25,34 +25,29 @@ class EsphomeI2cSniffer : public Component {
   void dump_config() override;
 
  protected:
-  // ISR helpers (called from trampolines in .cpp)
   void IRAM_ATTR on_scl_edge_();
   void IRAM_ATTR on_sda_edge_();
-
   void publish_frame_();
 
-  // Pins
   uint8_t sda_pin_{255};
   uint8_t scl_pin_{255};
 
-  // Entities
   text_sensor::TextSensor *msg_sensor_{nullptr};
   sensor::Sensor *last_addr_sensor_{nullptr};
   sensor::Sensor *last_data_sensor_{nullptr};
 
-  // State (shared with ISR)
   volatile bool in_transfer_{false};
   volatile bool last_sda_{true};
   volatile bool last_scl_{true};
 
-  volatile uint8_t bit_idx_{0};     // 0..7 within a byte, then one ACK bit
-  volatile bool ack_phase_{false};   // true while sampling ACK bit
+  volatile uint8_t bit_idx_{0};
+  volatile bool ack_phase_{false};
   volatile uint8_t cur_byte_{0};
 
   volatile bool have_addr_{false};
   volatile uint8_t addr_{0};
-  volatile bool rw_{false};          // false=W, true=R
-  uint8_t data_[64];                 // simple ringless buffer
+  volatile bool rw_{false};
+  uint8_t data_[64];
   volatile uint8_t data_len_{0};
 
   volatile bool frame_ready_{false};
