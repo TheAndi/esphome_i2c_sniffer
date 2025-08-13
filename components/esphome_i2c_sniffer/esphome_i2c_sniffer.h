60467ee1d22e6b9a7d340076c91912077ff84b83
TheAndi <33332336+TheAndi@users.noreply.github.com>
2025-08-13 23:39:53 +0200

    Update esphome_i2c_sniffer.h

--- components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
+++ components/esphome_i2c_sniffer/esphome_i2c_sniffer.h
@@ -1,62 +1,61 @@
 #pragma once
 
+#include <vector>
+#include <string>
 #include "esphome/core/component.h"
-#include "esphome/core/automation.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/log.h"
 #include "esphome/text_sensor/text_sensor.h"
 #include "esphome/sensor/sensor.h"
-
-#define MAX_BLOCKS 4
 
 namespace esphome {
 namespace esphome_i2c_sniffer {
 
-class AddressTrigger : public esphome::Trigger<uint8_t, std::vector<uint8_t>> {
- public:
-  void trigger_(uint8_t address, const std::vector<uint8_t> &data) {
-    this->publish(std::make_tuple(address, data));
-  }
-};
-
 class EsphomeI2cSniffer : public Component {
  public:
-  void set_sda_pin(uint8_t pin) { sda_pin_ = pin; }
-  void set_scl_pin(uint8_t pin) { scl_pin_ = pin; }
-  void register_msg_sensor(text_sensor::TextSensor *sensor) { msg_sensor_ = sensor; }
-  void register_addr_sensor(sensor::Sensor *sensor) { last_addr_sensor_ = sensor; }
-  void register_data_sensor(sensor::Sensor *sensor) { last_data_sensor_ = sensor; }
-  void add_on_address_trigger(AddressTrigger *trigger) { this->address_triggers_.push_back(trigger); }
+  void set_sda_pin(uint8_t pin) { this->sda_pin_ = pin; }
+  void set_scl_pin(uint8_t pin) { this->scl_pin_ = pin; }
+
+  void set_msg_sensor(text_sensor::TextSensor *s) { this->msg_sensor_ = s; }
+  void set_last_addr_sensor(sensor::Sensor *s) { this->last_addr_sensor_ = s; }
+  void set_last_data_sensor(sensor::Sensor *s) { this->last_data_sensor_ = s; }
 
   void setup() override;
   void loop() override;
+  void dump_config() override;
 
  protected:
-  static void IRAM_ATTR on_scl();
-  static void IRAM_ATTR on_sda();
-  void publish_blocks_();
+  // ISR helpers (called from trampolines in .cpp)
+  void IRAM_ATTR on_scl_edge_();
+  void IRAM_ATTR on_sda_edge_();
 
-  struct Block {
-    uint8_t addr;
-    char addr_ack;
-    char mode;
-    uint8_t data[32];
-    char data_ack[32];
-    uint8_t data_len;
-  };
+  void publish_frame_();
 
-  static volatile Block blocks_[MAX_BLOCKS];
-  static volatile uint8_t block_count_;
-  static volatile bool tr_ready_;
-  static volatile uint8_t bit_count_;
-  static volatile uint8_t byte_count_;
-  static volatile uint8_t data_byte_;
-  static volatile bool in_transfer_;
+  // Pins
+  uint8_t sda_pin_{255};
+  uint8_t scl_pin_{255};
 
-  uint8_t sda_pin_;
-  uint8_t scl_pin_;
+  // Entities
   text_sensor::TextSensor *msg_sensor_{nullptr};
   sensor::Sensor *last_addr_sensor_{nullptr};
   sensor::Sensor *last_data_sensor_{nullptr};
-  std::vector<AddressTrigger*> address_triggers_;
+
+  // State (shared with ISR)
+  volatile bool in_transfer_{false};
+  volatile bool last_sda_{true};
+  volatile bool last_scl_{true};
+
+  volatile uint8_t bit_idx_{0};     // 0..7 within a byte, then one ACK bit
+  volatile bool ack_phase_{false};   // true while sampling ACK bit
+  volatile uint8_t cur_byte_{0};
+
+  volatile bool have_addr_{false};
+  volatile uint8_t addr_{0};
+  volatile bool rw_{false};          // false=W, true=R
+  uint8_t data_[64];                 // simple ringless buffer
+  volatile uint8_t data_len_{0};
+
+  volatile bool frame_ready_{false};
 };
 
 }  // namespace esphome_i2c_sniffer
